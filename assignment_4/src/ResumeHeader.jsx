f6fe2aac3c2c830b095d7f7af0abef5d0c3ca795
Anushka2324 <155560984+Anushka2324@users.noreply.github.com>
2025-11-14 12:35:15 +0530

    Update ResumeHeader.jsx

--- assignment_4/src/ResumeHeader.jsx
+++ assignment_4/src/ResumeHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ResumeHeader() {
   return (
     <header>
-      <h1>Arya Hulwan</h1>
+      <h1>Anushka Patil</h1>
       <p>Final Year Computer Science Student | Aspiring Software Developer</p>
       <p>Email: aryahulwan@gmail.com | Phone: +91-9172192328</p>
     </header>
