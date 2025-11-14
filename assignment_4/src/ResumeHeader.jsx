52e854c78a6103e110bed9f9efb807ec17a8517e
Anushka2324 <155560984+Anushka2324@users.noreply.github.com>
2025-11-14 17:08:57 +0530

    final update

--- assignment_4/src/ResumeHeader.jsx
+++ assignment_4/src/ResumeHeader.jsx
@@ -5,7 +5,7 @@ function ResumeHeader() {
     <header>
       <h1>Anushka Patil</h1>
       <p>Final Year Computer Science Student | Aspiring Software Developer</p>
-      <p>Email: aryahulwan@gmail.com | Phone: +91-9172192328</p>
+      <p>Email: anushka2328@gmail.com | Phone: +91-9172192328</p>
     </header>
   );
 }
